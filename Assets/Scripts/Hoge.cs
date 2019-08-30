3535301ba71d0cfdd2f79c565298c90ed34902e1
Yosuke Nakano <ynakano@moff.mobi>
2019-08-30 16:58:16 +0900

    add scripts

---
+++ Assets/Scripts/Hoge.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniTed {
+    public class Hoge : IHoge {
+        public void HogeHoge() { Debug.Log("Hoge"); }
+    }
+}
